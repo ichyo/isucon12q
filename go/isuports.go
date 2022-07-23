35b78b11fdc6fb97a6d63bd478562b15dc20a5fa
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2022-07-23 15:54:34 +0900

    fix

--- go/isuports.go
+++ go/isuports.go
@@ -577,7 +577,7 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 		}
 	}
 
-	e.Logger.Infof("tenant_id = %d, competition_id = %s, playerYen = %d, visitorYen = %d, billingYen = %d", tenantID, competitionID, 100*playerCount, 10*visitorCount, 100*playerCount+10*visitorCount)
+	e.Logger.Infof("tenant_id = %d, competition_id = %s, playerYen = %d, visitorYen = %d, billingYen = %d", tenantID, competitonID, 100*playerCount, 10*visitorCount, 100*playerCount+10*visitorCount)
 
 	return &BillingReport{
 		CompetitionID:     comp.ID,
