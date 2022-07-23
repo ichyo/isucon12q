a1bb359253c8d6ad38101dd20319080a45328dc7
ichyo <ichyo00@gmail.com>
2022-07-23 13:00:02 +0900

    batch

--- go/isuports.go
+++ go/isuports.go
@@ -1086,18 +1086,26 @@ func competitionScoreHandler(c echo.Context) error {
 	); err != nil {
 		return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	}
-	for _, ps := range playerScoreRows {
-		if _, err := tenantDB.NamedExecContext(
-			ctx,
-			"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
-			ps,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
-				ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
-			)
 
+	valueQuery := ""
+	values := make([]interface{}, 0)
+	for _, ps := range playerScoreRows {
+		if valueQuery != "" {
+			valueQuery += ","
 		}
+		valueQuery += "(?, ?, ?, ?, ?, ?, ?, ?)"
+		values = append(values, ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt)
+	}
+
+	if _, err := tenantDB.ExecContext(
+		ctx,
+		"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at) VALUES "+valueQuery,
+		values...,
+	); err != nil {
+		return fmt.Errorf(
+			"error Insert player_scores: %w",
+			err,
+		)
 	}
 
 	return c.JSON(http.StatusOK, SuccessResult{
