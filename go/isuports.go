12e87a3c8412d24ea03d3d4e5157b68fef59fc3d
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2022-07-23 15:53:29 +0900

    fix

--- go/isuports.go
+++ go/isuports.go
@@ -576,6 +576,9 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 			}
 		}
 	}
+
+	e.Logger.Infof("tenant_id = %d, competition_id = %s, playerYen = %d, visitorYen = %d, billingYen = %d", tenantID, competitionID, 100*playerCount, 10*visitorCount, 100*playerCount+10*visitorCount)
+
 	return &BillingReport{
 		CompetitionID:     comp.ID,
 		CompetitionTitle:  comp.Title,
