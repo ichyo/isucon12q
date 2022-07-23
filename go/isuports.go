5ff44db8e428a0a40d05a658562ef996b732f8f3
ichyo <ichyo00@gmail.com>
2022-07-23 12:06:50 +0900

    sql index

--- go/isuports.go
+++ go/isuports.go
@@ -1590,6 +1590,30 @@ type InitializeHandlerResult struct {
 // データベースの初期化などが実行されるため、スキーマを変更した場合などは適宜改変すること
 func initializeHandler(c echo.Context) error {
 	out, err := exec.Command(initializeScript).CombinedOutput()
+
+	for i := 1; i < 100; i++ {
+		db, err := connectToTenantDB(int64(i))
+		if err != nil {
+			return fmt.Errorf("error connect:%e", err)
+		}
+		_, err = db.Exec("CREATE INDEX idx11 ON player_score (tenant_id, competition_id, player_id, row_num)")
+		if err != nil {
+			return fmt.Errorf("error index: %e", err)
+		}
+		_, err = db.Exec("CREATE INDEX idx12 ON player_score (tenant_id, competition_id, row_num")
+		if err != nil {
+			return fmt.Errorf("error index: %e", err)
+		}
+		_, err = db.Exec("CREATE INDEX idx2 ON competition (tenant_id, created_at)")
+		if err != nil {
+			return fmt.Errorf("error index: %e", err)
+		}
+		_, err = db.Exec("CREATE INDEX idx3 ON player (tenant_id, created_at)")
+		if err != nil {
+			return fmt.Errorf("error index: %e", err)
+		}
+	}
+
 	if err != nil {
 		return fmt.Errorf("error exec.Command: %s %e", string(out), err)
 	}
