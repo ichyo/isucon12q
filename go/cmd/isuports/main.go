758ec15ac93eda77dc4c68b8abb39e6113794853
ichyo <ichyo00@gmail.com>
2022-07-23 11:18:05 +0900

    Add profiler

--- go/cmd/isuports/main.go
+++ go/cmd/isuports/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/felixge/fgprof"
 	isuports "github.com/isucon/isucon12-qualify/webapp/go"
 )
 
 func main() {
+	http.DefaultServeMux.Handle("/debug/fgprof", fgprof.Handler())
+	go func() {
+		log.Print(http.ListenAndServe(":6060", nil))
+	}()
+
 	isuports.Run()
 }
