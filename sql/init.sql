420fd342ae05d57b0480c242ac7025ac2bc31ffe
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2022-07-23 15:11:33 +0900

    fix

--- sql/init.sql
+++ sql/init.sql
@@ -3,6 +3,10 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 DELETE FROM id_generator;
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
+DELETE FROM player_score WHERE created_at > '1654041599';
+DELETE FROM player WHERE created_at > '1654041548';
+DELETE FROM competition WHERE created_at > '1654039720';
+
 -- CREATE INDEX idx11 ON player_score (tenant_id, competition_id, player_id, row_num);
 -- CREATE INDEX idx12 ON player_score (tenant_id, competition_id, row_num);
 -- CREATE INDEX idx2 ON competition (tenant_id, created_at);
